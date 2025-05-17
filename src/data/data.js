1cf41df6a454ccb149a9049b826b1c53145be1e1
SMFaysalHaque <smfaysalhaque007@gmail.com>
2025-05-17 15:35:13 +0600

    feat: add data.

---
+++ src/data/data.js
@@ -0,0 +1,75 @@
+export const items = [
+  {
+    id: 1,
+    food_name: "Hamburger",
+    price: 300,
+    imgUrl: "./assets/hamburger.svg",
+  },
+  {
+    id: 2,
+    food_name: "Chicken Nuggets",
+    price: 400,
+    imgUrl: "./assets/chicken.svg",
+  },
+  {
+    id: 3,
+    food_name: "Submarine Sandwich",
+    price: 200,
+    imgUrl: "./assets/submarine.svg",
+  },
+  {
+    id: 4,
+    food_name: "Pizza slices",
+    price: 500,
+    imgUrl: "./assets/pizza.svg",
+  },
+  {
+    id: 5,
+    food_name: "French fry",
+    price: 100,
+    imgUrl: "./assets/frenchFry.jpg",
+  },
+  {
+    id: 6,
+    food_name: "Noodles",
+    price: 150,
+    imgUrl: "./assets/noodles.jpg",
+  },
+  {
+    id: 7,
+    food_name: "Ramen",
+    price: 350,
+    imgUrl: "./assets/ramen.jpg",
+  },
+];
+
+export const orders = [
+  {
+    orderId: 1,
+    name: "Sumit Saha",
+    itemCount: 5,
+    totalAmount: 500,
+    orderStatus: "PENDING",
+  },
+  {
+    orderId: 2,
+    name: "Akash Ahmed",
+    itemCount: 5,
+    totalAmount: 700,
+    orderStatus: "DELIVERED",
+  },
+  {
+    orderId: 3,
+    name: "Saad Hasan",
+    itemCount: 5,
+    totalAmount: 400,
+    orderStatus: "PENDING",
+  },
+  {
+    orderId: 4,
+    name: "MD Salahuddin",
+    itemCount: 5,
+    totalAmount: 600,
+    orderStatus: "PENDING",
+  },
+];
